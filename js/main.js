eacded769e0ee36a3a5420ae1213b0688f614236
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2021-03-27 13:09:15 -0600

    obtener el valor de un select

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(() => {           //Estructura basica para usar jQuery; el evento ready ejecutara su funcion anonima cuando el documento este completamente cargado
-    $('#my-form').val('Valor robable')  //Con el metodo .val() podemos ingresar un valor que pasemos por parametro.
-    $('#btn-steal').click(() => {    //Seleccionamos por ID nuestro boton y mediante el evento click lanzamos un alert
-        alert(`Me robe esto: ${$('#my-form').val()}`)   //Tambien con .val() podemos obtener el valor del input que contenga el Id seleccionado
+    $('#my-btn').click(() => {
+        alert(`El valor seleccionado es ${$('#my-select').val()}`)  //De esta manera podemos obtener en valor de la opcion elegida en nuestro select
     });
 });
