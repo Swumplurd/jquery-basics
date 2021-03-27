915765c5cf4d46a10ac93820833752cf998af9cb
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2021-03-27 13:18:31 -0600

    saber si un checkbox esta checked

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(() => {           //Estructura basica para usar jQuery; el evento ready ejecutara su funcion anonima cuando el documento este completamente cargado
-    $('#my-btn').click(() => {
-        alert(`El valor seleccionado es ${$('#my-select').val()}`)  //De esta manera podemos obtener en valor de la opcion elegida en nuestro select
+    $('#check').click(() => {
+        if ($('#my-check').is(':checked')) {    //Preguntamos si el checkbox con el id esta checkeado
+            alert('Lo palomeaste')      //Si lo esta lanzamos este alert
+        } else {
+            alert('No lo palomeaste')   //Si no lo esta lanzamos este alert
+        }
     });
 });
