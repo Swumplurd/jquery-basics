8f6a3ab0a450ab57ff4ec366c7bae3e057212078
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2021-03-27 12:52:53 -0600

    propiedad val con jquery

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 $(document).ready(() => {           //Estructura basica para usar jQuery; el evento ready ejecutara su funcion anonima cuando el documento este completamente cargado
-    $('#btn-nuke').click(() => {    //Seleccionamos por ID nuestro boton y mediante el evento click lanzamos un alert
-        $('#my-div').text('Corre !!, corre por tu vida !!');    //Colocamos el texto pasado por parametro al div
-        /* 
-            El metodo .text() funciona de dos maneras diferentes
-            si le pasamos como parametrouna cadena pasara ese texto
-            al elemento que estamos seleccionando con jquery, pero si
-            no le pasamos parametros nos taera el texto que contenga
-            dicho elemento
-        */
-       let text = $('#my-div').text();  //Obtenemos el texto que contiene el div
-       console.log(text);
+    $('#my-form').val('Valor robable')  //Con el metodo .val() podemos ingresar un valor que pasemos por parametro.
+    $('#btn-steal').click(() => {    //Seleccionamos por ID nuestro boton y mediante el evento click lanzamos un alert
+        alert(`Me robe esto: ${$('#my-form').val()}`)   //Tambien con .val() podemos obtener el valor del input que contenga el Id seleccionado
     });
 });
