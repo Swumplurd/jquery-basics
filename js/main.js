46ea6671ffc9c24cf5f5752078c7af829646e8b1
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2021-03-27 13:39:57 -0600

    saber si un radio esta checked(activo)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(() => {           //Estructura basica para usar jQuery; el evento ready ejecutara su funcion anonima cuando el documento este completamente cargado
-    $('#check').click(() => {
-        if ($('#my-check').is(':checked')) {    //Preguntamos si el checkbox con el id esta checkeado
-            alert('Lo palomeaste')      //Si lo esta lanzamos este alert
+    $('#my-btn').click(() => {
+        if ($('#rad-si').is(':checked')) {          //Comprobamos si el radio si esta activo
+            alert('El radio si esta activo')        //Si esta acrivo si lanzamos este alert
+        } else if ($('#rad-no').is(':checked')) {   //Comprobamos si el radio no esta activo
+            alert('El radio no esta activo')        //Si esta activo no lanzamos este alert
         } else {
-            alert('No lo palomeaste')   //Si no lo esta lanzamos este alert
+            alert('Ningun radio esta activo')       //Si ninguno esta activo lanzamos este alert
         }
     });
 });
