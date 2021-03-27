89457789ee977b9b76ee7d28350d6445cad5d5df
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2021-03-27 14:05:46 -0600

    conglomerado de funciones basicas de jquery

--- js/main.js
+++ js/main.js
@@ -1,11 +1,56 @@
 $(document).ready(() => {           //Estructura basica para usar jQuery; el evento ready ejecutara su funcion anonima cuando el documento este completamente cargado
+    eventoClick();
+    propText();
+    propVal();
+    selects();
+    checks();
+    radios()
+});
+
+function eventoClick() {
+    $('#event-click').click(() => {
+        alert('Este es el evento click')
+    });
+}
+
+function propText() {
+    $('#btn-txt').click(() => {
+        $('#my-div').text('Colocaste un Texto con el boton, ahora ve la consola');
+        console.log(`Obtubiste el texto: ${$('#my-div').text()}`);
+    });
+}
+
+function propVal() {
+    $('#my-form').val('Valor robable') 
+    $('#btn-steal').click(() => {
+        alert(`Me robe esto: ${$('#my-form').val()}`)
+    });
+}
+
+function selects() {
     $('#my-btn').click(() => {
-        if ($('#rad-si').is(':checked')) {          //Comprobamos si el radio si esta activo
-            alert('El radio si esta activo')        //Si esta acrivo si lanzamos este alert
-        } else if ($('#rad-no').is(':checked')) {   //Comprobamos si el radio no esta activo
-            alert('El radio no esta activo')        //Si esta activo no lanzamos este alert
+        alert(`El valor seleccionado es ${$('#my-select').val()}`)
+    });
+}
+
+function checks() {
+    $('#check').click(() => {
+        if ($('#my-check').is(':checked')) {
+            alert('Lo palomeaste')
         } else {
-            alert('Ningun radio esta activo')       //Si ninguno esta activo lanzamos este alert
+            alert('No lo palomeaste')
         }
     });
-});
+}
+
+function radios() {
+    $('#my-button').click(() => {
+        if ($('#rad-si').is(':checked')) {
+            alert('El radio si esta activo')
+        } else if ($('#rad-no').is(':checked')) {
+            alert('El radio no esta activo')
+        } else {
+            alert('Ningun radio esta activo')
+        }
+    });
+}
